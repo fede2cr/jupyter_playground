58a64bbbc02e7fff2d32c623f10b35d813be44fb
Alvaro Figueroa Cabezas <alvaro@greencore.co.cr>
2017-06-19 11:44:52 -0600

    Usuario greencore como variable, para poder ejecutar desde root

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 # Instalador de Jupyter notebook para ser usado en ambiente educativo
 # Para cursos en Greencore Solutions
 
+user=greencore
+
 # Tareas administrativas, instalación de paquetes
 sudo apt-get install python3-pip
 sudo pip3 install --upgrade pip
@@ -11,10 +13,10 @@ sudo python3 -m bash_kernel.install
 # Creando configuración desde cuenta del usuario, clave "greencore"
 
 jupyter notebook --generate-config
-echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc' >> ~/.jupyter/jupyter_notebook_config.py"
+echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc' >> ~${user}/.jupyter/jupyter_notebook_config.py"
 
 # Para permisos con sudo
 echo '%sudo ALL=(ALL) NOPASSWD: ALL'  | sudo tee /etc/sudoers.d/jupyter
-echo "Defaults:${USER} !requiretty" | sudo tee -a /etc/sudoers.d/jupyter
+echo "Defaults:${user} !requiretty" | sudo tee -a /etc/sudoers.d/jupyter
 sudo chmod 0440 /etc/sudoers.d/jupyter
 

45f05a6edc40668d4828dd5cd7decf63f5c6a36c
Alvaro Figueroa Cabezas <alvaro@greencore.co.cr>
2017-06-19 13:24:32 -0600

    install.sh sin sudo para probarsh...

--- install.sh
+++ install.sh
@@ -7,19 +7,19 @@ set +x
 user=greencore
 
 # Tareas administrativas, instalación de paquetes
-sudo apt-get install -y python3-pip
-sudo pip3 install --upgrade pip
-sudo pip3 install jupyter bash_kernel
-sudo python3 -m bash_kernel.install
+apt-get install -y python3-pip
+pip3 install --upgrade pip
+pip3 install jupyter bash_kernel
+python3 -m bash_kernel.install
 
 # Creando configuración desde cuenta del usuario, clave "greencore"
 
 sudo -u ${user} jupyter notebook --generate-config
 sleep 5s
-echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc'" >> ~${user}/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc'" | tee -a ~${user}/.jupyter/jupyter_notebook_config.py
 
 # Para permisos con sudo
-echo '%sudo ALL=(ALL) NOPASSWD: ALL'  | sudo tee /etc/sudoers.d/jupyter
-echo "Defaults:${user} !requiretty" | sudo tee -a /etc/sudoers.d/jupyter
-sudo chmod 0440 /etc/sudoers.d/jupyter
+echo '%sudo ALL=(ALL) NOPASSWD: ALL'  | tee /etc/sudoers.d/jupyter
+echo "Defaults:${user} !requiretty" | tee -a /etc/sudoers.d/jupyter
+chmod 0440 /etc/sudoers.d/jupyter
 

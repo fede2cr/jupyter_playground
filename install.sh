1dc500c304e50024b1540c388dfb117181ddb5c5
Alvaro Figueroa Cabezas <alvaro@greencore.co.cr>
2017-06-19 12:40:23 -0600

    Cambios menores en install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 user=greencore
 
 # Tareas administrativas, instalación de paquetes
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install jupyter bash_kernel
 sudo python3 -m bash_kernel.install
@@ -13,7 +13,7 @@ sudo python3 -m bash_kernel.install
 # Creando configuración desde cuenta del usuario, clave "greencore"
 
 jupyter notebook --generate-config
-echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc' >> ~${user}/.jupyter/jupyter_notebook_config.py"
+echo "c.NotebookApp.password = 'sha512:8c79bf18770a:e874ca1ba53f2538c308830430211c3604fb4da03feefc64280584b690ff41448445490e38a697ba29049f69d003a5c2bf70bfed547e9fb6858f06ba202774bc'" >> ~${user}/.jupyter/jupyter_notebook_config.py
 
 # Para permisos con sudo
 echo '%sudo ALL=(ALL) NOPASSWD: ALL'  | sudo tee /etc/sudoers.d/jupyter
